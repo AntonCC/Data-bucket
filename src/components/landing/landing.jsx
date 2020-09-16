39d84ea1e02578a99cb983d2d09e0613538a2676
Anton Chetvertkov <antonchetvv@gmail.com>
2020-09-16 12:29:21 -0400

    testing landing, might use

---
+++ src/components/landing/landing.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './landing.scss'
+import Button from '../button/button'
+
+const Landing = () => {
+  return (
+    <div className="landing">
+      <div className="container">
+        <div className="text">
+          <h1>Reduce the size of large files with Data Bucket's online compressor</h1>
+          <h2>No downloads are necessary! Manage your files in the browser, and quickly share them.</h2>
+          <Button buttonStyle="btn-gradient" link='./pricing'>Try Data Bucket</Button>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default Landing
