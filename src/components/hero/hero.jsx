fe7f574fbf1ae7271d21decbe71676beba6edfab
Anton <antonchetvv@gmail.com>
2021-01-28 11:52:29 -0500

    chaged hero font size and height

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './hero.scss'
 import { motion } from 'framer-motion'
 import Button from '../../components/button/button'
 
 const Hero = ({ title, subtitle, buttonText, image, video, extend, link }) => {
+  const [windowHeight, setWindowHeight] = useState(window.innerHeight)
+
+  useEffect(() => {
+    window.addEventListener('resize', handleHeightChange)
+
+    return () => {
+      window.removeEventListener('resize', handleHeightChange)
+    }
+  }, [])
+
+  const handleHeightChange = () => {
+    setWindowHeight(window.innerHeight)
+  }
+
   return (
-    <div className={`hero ${extend ? 'extend' : ''}`}>
+    <div className={`hero ${extend ? 'extend' : ''}`} style={{ height: `calc(${windowHeight})px` }}>
       <div className="container">
         <motion.div className="side-a" 
           initial={ { opacity: 0, y: -25} } 
